a3064b99896fb7d1051121e63a0ad5c7d2b3e646
hirokazu1209 <n.hirokazu92@gmail.com>
2021-03-03 21:56:30 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html
+}
+
 function post (){
   // getElementByIdメソッドで取得した投稿ボタンの要素を変数submitに格納
   const submit = document.getElementById("submit");
@@ -11,11 +25,23 @@ function post (){
     // 新たに生成したXMLHttpRequestオブジェクトを変数XHRに格納
     const XHR = new XMLHttpRequest();
     // 第一引数にはHTTPメソッド、第二引数にはパス、第三引数には非同期通信であるかをtrueかfalseで記述
-    XHR.open("POST", "/posts", true);
+    XHR.open("POST", "/posts", true); 
     // データフォーマットを「JSON」に指定
     XHR.responseType = "json";
     // フォームに入力された内容をサーバー側に送信
     XHR.send(formData);
+    // レスポンスの受信に成功したときの処理
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }      
+      const list = document.getElementById("list");
+      // リセットの対象となるフォームの要素contentを取得
+      const formText = document.getElementById("content");      
+      list.insertAdjacentHTML("afterend", buildHTML(XHR)); 
+      formText.value = "";             
+    };
   });
 }
 
